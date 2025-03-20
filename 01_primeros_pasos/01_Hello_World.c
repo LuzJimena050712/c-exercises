80ae48009580826044bd238c6e659365411fa118
LuzJimena050712 <luzjimenarcegabriel@gmail.com>
2025-03-19 20:50:47 -0600

    Ejercicios primeros pasos de C

---
+++ 01_primeros_pasos/01_Hello_World.c
@@ -0,0 +1,12 @@
+// Incluimos las bibliotecas necesarias (entrada - salida)
+#include <stdio.h>
+
+// Función principal
+int main(){
+    //Imprimir nuestro mensaje
+    printf("Hello world \n");
+
+    // Indicar que el programa finalizo correctamente
+
+    return 0;
+}
---
+++ 01_primeros_pasos/03_Operaciones_Matematicas.c
@@ -0,0 +1,28 @@
+//Importamos la biblioteca (entrada-salida)
+#include <stdio.h>
+//Biblioteca para operaciones
+#include <stdlib.h>
+
+//Funcion principal.
+
+int main(){
+    //Para numeros enteros %d /f (float)
+
+    //Imprimir un numero entero.
+
+    printf("%d \n", 3);
+
+    //Imprimir la suma de dos números de forma directa.
+
+    printf("%d \n", 3 + 4); 
+
+    //Imprimir la suma con un mensaje.
+
+    printf("La suma de %d + %d = %d \n", 3 , 4 , 3 + 4);
+
+    printf("La suma de %d + %d = %d \n", 5, 8, 5 + 8 );
+
+    printf("La suma de %d + %d = %d \n", 78787, 3259, 78787 + 3259);
+
+    return 0;
+}
