639d81de0dbf109291851c05c0635820a69a062c
LuzJimena050712 <luzjimenarcegabriel@gmail.com>
2025-03-19 20:23:05 -0600

    Ejercicios sobre tipos de datos en C

---
+++ 01_Constantes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+ //Definir una constante.
+
+ #define PI 3.1416
+
+ int main(){
+    //Imprimir el valor de la constante.
+
+    printf("PI es igual %f \n", PI);
+    
+    return 0;
+ }
