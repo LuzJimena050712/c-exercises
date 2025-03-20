05092fa7720ce4415deeb6d36d2d165da45b0a64
LuzJimena050712 <luzjimenarcegabriel@gmail.com>
2025-03-19 20:35:01 -0600

    Ejercicios sobre tipos de datos en C

---
+++ 02_Print_Formated.c
@@ -0,0 +1,22 @@
+//Incluimos la biblioteca (entrada-salida).
+#include <stdio.h>
+//Función principal
+int main(){
+    //Imprimir un mensaje con salto de linea.
+    printf("Este es un texto \n");
+
+    //Imprimir una linea normal
+    printf("Mi novio es Ivan Mora");
+
+    //Imprimir un salto para separar lineas.
+    printf("\n");
+
+    //Imprimir una linea normal (nuevamente)
+
+    printf("Soy todo un programador");
+
+    //Imprimir otro salto de linea.
+    printf("\n");
+    
+
+}
---
+++ 02_Tipos_de_Datos/02_Declarar_Datos.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+int main(){
+
+    //Declarar variables enteras.
+    int unNumero = 3;
+    int otroNumero = 4;
+    
+    printf("%d \n", unNumero + otroNumero); 
+
+    return 0;
+
+}
